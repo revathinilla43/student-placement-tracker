346c5389fe41adb9b7db7645121339a46e2d916c
revathinilla43 <revathinilla43@gmail.com>
2026-06-03 16:05:14 +0530

    Added student search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import StudentList from "./components/StudentList";
 
 function App(){
   const[students,setStudents]=useState([]);
+  const[search,setSearch]=useState("");
   return(
     <>
       <h1>Student Placement Tracker</h1>
@@ -14,8 +15,16 @@ function App(){
         setStudents={setStudents} 
       />
       <p>Total Students:{students.length}</p> 
+
+      <input 
+        type="text" 
+        placeholder="Search students..." 
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+      />
       <StudentList students={students}
-                   setStudents={setStudents}/>
+                   setStudents={setStudents}
+                   search={search}/>
 
     </>
   );
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,14 +1,20 @@
 
-function StudentList({students,setStudents}){
+function StudentList({students,setStudents,search}){
+
     const deleteStudent=(indexToDelete)=>{
         const updatedStudents=students.filter((_,index)=>index!==indexToDelete);
         setStudents(updatedStudents);
     };
-    
+
+    const filteredStudents=students.filter(student=>
+        student.name.toLowerCase().includes(search.toLowerCase()) ||
+        student.rollNo.toLowerCase().includes(search.toLowerCase())
+    );
+     
     return (
     <>
     <h2>Student List</h2>
-    {students.map((student,index)=>(
+    {filteredStudents.map((student,index)=>(
 
     <div key={index}>
         <p>
